5f2e8a73c7e0d783da0597a3be4f05a18f1c6c6c
BlakeleyKilgore <bkilgore@digium.com>
2018-05-01 18:05:48 -0500

    Make dynamic blank player board

---
+++ src/minesweeper.js
@@ -0,0 +1,12 @@
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let r = 0; r < numberOfRows; r++) {
+    let row = [];
+    for (let c = 0; c < numberOfColumns; c++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+};
+console.log(generatePlayerBoard(3,3));
